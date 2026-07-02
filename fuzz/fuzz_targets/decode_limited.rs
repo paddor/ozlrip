0b7d340bfd946d9b7dcd18472986760acddb9dba
Patrik Wenger <paddor@gmail.com>
2026-07-02 19:22:07 +0200

    Add decode fuzz targets

---
+++ fuzz/fuzz_targets/decode_limited.rs
@@ -0,0 +1,22 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use ozlrip::Limits;
+
+fuzz_target!(|data: &[u8]| {
+    let limits = Limits {
+        max_frame_bytes: 4096,
+        max_decoded_bytes: 4096,
+        max_chunks: 16,
+        max_nodes: 256,
+        max_streams: 256,
+        max_transform_header_bytes: 4096,
+        max_stored_stream_bytes: 4096,
+        max_buffer_bytes: 4096,
+        max_graph_depth: 64,
+        max_expansion_ratio: 64,
+    };
+    let mut output = Vec::new();
+    let _ = ozlrip::decode_into(data, &mut output, limits);
+});
+
